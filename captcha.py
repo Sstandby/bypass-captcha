00ad750d896c4494d73209d3b5b9fc0cb55ff2ec
Sstandby <standbyy9@gmail.com>
2022-02-18 17:32:42 -0500

    1.0.1

--- captcha.py
+++ captcha.py
@@ -7,18 +7,21 @@ import pathlib
 import pytesseract
 
 #ABCDEFGHIJKMNLOPKRSTUVWXYZ1
-
+contador = 0
 def resolve(img):
     os.system(f"convert /home/gatete/Workspace/MM-S/auto-captcha/imagenes/captcha/{img} -morphology Erode Disk:2 captcha5.tif")
     os.system("convert captcha5.tif -morphology Dilate Disk:1.5 captcha5.tif")
     os.system("convert captcha5.tif -threshold 41% captcha5.tif")
     os.system("convert captcha5.tif -level %70,100% captcha5.tif")
-    captcha = pytesseract.image_to_string(Image.open('captcha5.tif'),config='--oem 0 --dpi 70 --psm 8 -c tessedit_char_whitelist=0123456789')
+    captcha = pytesseract.image_to_string(Image.open('captcha5.tif'),config='--oem 0 --psm 6 --dpi 70 -c tessedit_char_whitelist=0123456789')
     captcha = captcha.replace(" ", "").strip()
-    print(captcha)
-
+    return captcha
 dir = '/home/gatete/Workspace/MM-S/auto-captcha/imagenes/captcha'
 directorio = pathlib.Path(dir)
 for fichero in directorio.iterdir():
-    print(fichero.name)
-    resolve(fichero.name)
+    numero = resolve(fichero.name)
+    temp = fichero.name.replace(".png", "").strip()
+    print("Numero del resolver: ", numero, " - Numero del archivo: ", temp)
+    if numero != '' and int(numero) == int(temp):
+       contador += 1
+print(contador)

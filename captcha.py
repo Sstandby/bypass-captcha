965e256ca0b5feb6bdfaceba8a6696b02d0b14c0
Sstandby <standbyy9@gmail.com>
2022-02-19 09:01:02 -0500

    1.0.1

--- captcha.py
+++ captcha.py
@@ -3,25 +3,30 @@ try:
 except ImportError:
         import Image
 import os
+import re
 import pathlib
 import pytesseract
 
 #ABCDEFGHIJKMNLOPKRSTUVWXYZ1
 contador = 0
-def resolve(img):
-    os.system(f"convert /home/gatete/Workspace/MM-S/auto-captcha/imagenes/captcha/{img} -morphology Erode Disk:2 captcha5.tif")
-    os.system("convert captcha5.tif -morphology Dilate Disk:1.5 captcha5.tif")
-    os.system("convert captcha5.tif -threshold 41% captcha5.tif")
-    os.system("convert captcha5.tif -level %70,100% captcha5.tif")
-    captcha = pytesseract.image_to_string(Image.open('captcha5.tif'),config='--oem 0 --psm 6 --dpi 70 -c tessedit_char_whitelist=0123456789')
-    captcha = captcha.replace(" ", "").strip()
-    return captcha
 dir = '/home/gatete/Workspace/MM-S/auto-captcha/imagenes/captcha'
 directorio = pathlib.Path(dir)
+
+def resolve(img):
+    os.system(f"convert {dir}/{img} -morphology Erode Disk:2.2 captcha5.tif")
+    os.system("convert captcha5.tif -morphology Dilate Disk:1.2 captcha5.tif")
+    os.system("convert captcha5.tif -gaussian-blur 0 -threshold 41% -paint 1 captcha5.tif")
+    captcha = pytesseract.image_to_string(Image.open('captcha5.tif'),config='--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789')
+    captcha = captcha.replace(" ", "").strip()
+    return captcha
+
 for fichero in directorio.iterdir():
     numero = resolve(fichero.name)
     temp = fichero.name.replace(".png", "").strip()
     print("Numero del resolver: ", numero, " - Numero del archivo: ", temp)
     if numero != '' and int(numero) == int(temp):
        contador += 1
+    #else:
+    #: numero.re
+
 print(contador)
